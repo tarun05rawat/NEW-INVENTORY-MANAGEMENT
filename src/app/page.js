ef90f0762ee2b6881209c6797e65f64acf4e646f
Tarun Rawat <144971861+tarun05rawat@users.noreply.github.com>
2024-08-04 00:52:18 +0400

    final commit

--- src/app/page.js
+++ src/app/page.js
@@ -29,6 +29,7 @@ import {
   getDoc,
 } from "firebase/firestore";
 
+NEXT_PUBLIC_SPOONACULAR_API_KEY = "074a45a2489e444bab35e496e5d8e140";
 export default function Home() {
   const [inventory, setInventory] = useState([]);
   const [open, setOpen] = useState(false);
@@ -144,7 +145,7 @@ export default function Home() {
       const ingredients = selectedItems.join(",+");
       console.log("Selected Ingredients: ", ingredients); // Debugging
       const response = await axios.get(
-        `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredients}&number=5&apiKey=${process.env.NEXT_PUBLIC_SPOONACULAR_API_KEY}`
+        `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredients}&number=5&apiKey=${NEXT_PUBLIC_SPOONACULAR_API_KEY}`
       );
       console.log("API Response: ", response.data); // Debugging
       setRecipes(response.data);
